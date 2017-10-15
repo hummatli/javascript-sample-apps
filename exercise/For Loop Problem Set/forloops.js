49a06e16a304e2fb99f3edca6aeaf3f81967e614
Sattar Hummatli <settarxan@gmail.com>
2017-10-15 19:59:05 +0400

    Made exercise For Loop

---
+++ exercise/For Loop Problem Set/forloops.js
@@ -0,0 +1,27 @@
+
+console.log("PRINTING NUMBERS BETWEAN -10 AND 19 ")
+for(var i = -10; i <= 19; i++) {
+	console.log(i)
+}
+
+
+
+console.log("PRINTING EVEN NUMBERS BETWEAN 10 AND 40 ")
+for (var i = 10; i <= 40; i+=2) {
+	console.log(i)
+}
+
+console.log("PRINTING ODD NUMBERS BETWEAN 300 AND 333 ")
+for (var i = 301; i <= 333; i+=2) {
+	console.log(i)
+}
+
+
+console.log("PRINTING ALL NUMBERS DIVISIBLE BY 5 AND 3 BETWEAN 5 AND 50 ")
+for (var i = 5; i <= 50; i+=1) {
+	if(i % 5 === 0 && i % 3 === 0) {
+		console.log(i)
+	}
+}
+
+

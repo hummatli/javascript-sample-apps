6c7f6922e9c080cfee711ca9c1b303febf0f8d76
Sattar Hummatli <settarxan@gmail.com>
2017-10-16 17:30:26 +0400

    Added new app called Todo List

---
+++ Todo List App/list.js
@@ -0,0 +1,20 @@
+var todos = []
+
+var input = ""
+
+while(input !== "quit") {
+
+	if(input === "list") {
+		console.log(todos)
+	} else if(input === "new") {
+		//ask for new todo
+		var newTodo = prompt("Enter new todo")
+		//add to todos array
+		todos.push(newTodo)
+
+	}
+	
+	input = prompt("What would you like to do?")
+}
+
+console.log("OK, YOU QUIT THE APP")

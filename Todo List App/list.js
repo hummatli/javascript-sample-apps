9bfcb456e2df5434eb9deb7999744d5160313dea
Sattar Hummatli <settarxan@gmail.com>
2017-10-17 10:53:55 +0400

    Edited Todo List App

--- Todo List App/list.js
+++ Todo List App/list.js
@@ -5,16 +5,39 @@ var input = ""
 while(input !== "quit") {
 
 	if(input === "list") {
-		console.log(todos)
+		listTodos()
 	} else if(input === "new") {
-		//ask for new todo
-		var newTodo = prompt("Enter new todo")
-		//add to todos array
-		todos.push(newTodo)
-
+		addTodo()
+	} else if(input === "delete") {
+		deleteTodo()
 	}
 	
 	input = prompt("What would you like to do?")
 }
 
 console.log("OK, YOU QUIT THE APP")
+
+
+function listTodos() {		
+	console.log("***********")
+	todos.forEach(function(todo, index){
+		console.log(index + ": " + todo)
+	})
+	console.log("***********")
+}
+
+function addTodo() {
+	//ask for new todo
+	var newTodo = prompt("Enter new todo")
+	//add to todos array
+	todos.push(newTodo)
+	console.log("Added Todo")
+}
+
+function deleteTodo() {
+	//ask for index to delete in list
+	var index = prompt("Enter index of todo to delete")
+	//delete that todo
+	todos.splice(index, 1)
+	console.log("Deleted Todo")
+}
